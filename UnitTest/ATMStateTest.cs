7880d0afddd546c123d43607e9cd17e3bbd26735
sreelux <112001042@smail.iitpkd.ac.in>
2023-09-15 22:00:45 +0530

    edited testcase

--- UnitTest/ATMStateTest.cs
+++ UnitTest/ATMStateTest.cs
@@ -34,24 +34,13 @@ namespace UnitTest
 
             var atm = new ATMContext( initialBalances );
 
-
-            // Test inserting a card
-            Assert.IsTrue(atm.InsertCard(), "Failed to insert card.");
-
-            // Test entering a PIN
-            Assert.IsTrue(atm.EnterPIN("1234") ,"Entered incorrect PIN.");
-
-            // Test checking the balance
-            Assert.AreEqual(2500, atm.CheckBalance(), "Balance check failed.");
-
-            // Test withdrawing cash
-            Assert.IsTrue(atm.WithDrawCash(500), "Failed to withdraw cash ");
-
-            // Test checking the updated balance
+            atm.InsertCard();
+            atm.EnterPIN( "1234" );
+            atm.WithDrawCash(500);
+           
             Assert.AreEqual(2000, atm.CheckBalance(), "Couldn't check balance");
 
-            // Test removing the card
-            Assert.IsTrue(atm.RemoveCard(), "Removal card not successful");
+            atm.RemoveCard();
 
             
         }
@@ -72,17 +61,12 @@ namespace UnitTest
             var atm = new ATMContext( initialBalances );
 
 
-            // Test inserting a card
-            Assert.IsTrue(atm.InsertCard(), "Failed to insert card.");
+            atm.InsertCard();
 
             // Test withdrawing cash
             Assert.IsFalse(atm.WithDrawCash(500), "Failed to withdraw cash with incorrect PIN.");
 
-            //Entering PIN and trying again
-            Assert.IsTrue(atm.EnterPIN("1234"), "Wrong PIN ");
-
-            //Now trying to withdraw cash
-            Assert.IsTrue(atm.WithDrawCash(500), "Failed to withdraw cash with incorrect PIN.");
+            
 
 
 
@@ -103,11 +87,8 @@ namespace UnitTest
 
             var atm = new ATMContext( initialBalances );
 
-            // Test inserting a card
-            Assert.IsTrue(atm.InsertCard(), "Failed to insert card.");
-
-            // Test entering a PIN
-            Assert.IsTrue(atm.EnterPIN("1234"), "Entered incorrect PIN.");
+            atm.InsertCard();
+            atm.EnterPIN( "1234" );
 
             // Test withdrawing cash
             Assert.IsFalse(atm.WithDrawCash(5000), "Failed to withdraw cash ");
@@ -132,9 +113,8 @@ namespace UnitTest
             var atm = new ATMContext( initialBalances );
 
 
-            // Test inserting a card
-            Assert.IsTrue(atm.InsertCard(), "Failed to insert card.");
-
+            atm.InsertCard();
+            
             // Test entering a PIN
             Assert.IsFalse(atm.EnterPIN("123"), "Entered incorrect PIN.");
 
@@ -155,28 +135,13 @@ namespace UnitTest
 
             var atm = new ATMContext( initialBalances );
 
-
-            // Test inserting a card
-            Assert.IsTrue(atm.InsertCard(), "Failed to insert card.");
-
+            atm.InsertCard();
+           
+           
             // Test inserting a card
             Assert.IsFalse(atm.InsertCard(), "Failed to insert card.");
 
-            // Test entering a PIN
-            Assert.IsFalse(atm.EnterPIN("12344"), "Entered incorrect PIN.");
-
-            // Test entering a PIN
-            Assert.IsTrue(atm.EnterPIN("1234"), "Entered incorrect PIN.");
-
-            // Test withdrawing cash
-            Assert.IsTrue(atm.WithDrawCash(500), "Failed to withdraw cash ");
-
-            // Test removing the card
-            Assert.IsTrue(atm.RemoveCard(), "Removal card not successful");
-
-            // Test checking the updated balance
-            Assert.AreNotEqual(2000, atm.CheckBalance(), "Couldn't check balance");
-
+           
 
         }
 
@@ -195,14 +160,10 @@ namespace UnitTest
 
             var atm = new ATMContext( initialBalances );
 
-            // Test inserting a card
-            Assert.IsTrue( atm.InsertCard() , "Failed to insert card." );
-
-            // Test entering a PIN
-            Assert.IsTrue( atm.EnterPIN( "1234" ) , "Entered incorrect PIN." );
-
-            // Test withdrawing cash
-            Assert.IsTrue( atm.WithDrawCash(1000) , "Failed to withdraw cash " );
+            atm.InsertCard();
+            atm.EnterPIN( "1234" );
+            atm.WithDrawCash( 1000 );
+            
             // Test withdrawing cash
             Assert.IsFalse( atm.WithDrawCash( 1500 ) , "Failed to withdraw cash2 " );
             
